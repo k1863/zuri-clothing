cfa0790354f5ca21e2a52c63976a5fd7033a99ae
k1863 <57972610+k1863@users.noreply.github.com>
2020-07-28 19:25:40 -0300

    added navcontainer component and links to home and shop

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/Shop/ShopPage.jsx";
+import NavContainer from "./components/NavContainer/NavContainer.jsx";
+
 function App() {
   return (
     <div>
+      <NavContainer />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
